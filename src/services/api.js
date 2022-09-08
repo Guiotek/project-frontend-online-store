95f4e31079a62b7d956bbb89bed75bf55a95d83a
Victor Flores <victorf300@gmail.com>
2022-09-08 15:41:17 -0300

    Requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,25 @@
 export async function getCategories() {
-  // Implemente aqui
+  const fetchCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const response = await fetchCategories.json();
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (categoryId && query) {
+    const fetchGetProductsFromCategoryAndQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+    const response = await fetchGetProductsFromCategoryAndQuery.json();
+    return response;
+  }
+  if (query) {
+    const fetchGetProductsFromCategoryAndQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const response = await fetchGetProductsFromCategoryAndQuery.json();
+    return response;
+  }
+  if (categoryId) {
+    const fetchGetProductsFromCategoryAndQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const response = await fetchGetProductsFromCategoryAndQuery.json();
+    return response;
+  }
 }
 
 export async function getProductById() {
