a6bce1d923186de97864d595160081d39ef0c551
Igor Arecippo <iarecippo@gmail.com>
2022-09-08 16:10:29 -0300

    subindo o requisito 2

---
+++ src/MainPage.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <h1
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+
+      </h1>
+    );
+  }
+}
